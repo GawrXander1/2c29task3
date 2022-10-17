b2fc64599374ae70daf8d523a66b1c59c5e9af69
GawrXander1 <115450405+GawrXander1@users.noreply.github.com>
2022-10-17 14:48:36 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.AB, function () {
+    count = 0
+})
+input.onButtonPressed(Button.B, function () {
+    count = 9
+    basic.showNumber(count)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
+    }
     basic.showLeds(`
         . . . . .
         . . . . .
